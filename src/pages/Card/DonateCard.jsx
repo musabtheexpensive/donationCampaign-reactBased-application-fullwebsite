856076bb816d53a9d8a31da86be6103971878a35
Musab Al Mahi <musabalmahi53@gmail.com>
2023-10-07 13:21:52 +0600

    fixed the issue that i am ever seen

--- src/pages/Card/DonateCard.jsx
+++ src/pages/Card/DonateCard.jsx
@@ -29,21 +29,7 @@ const DonateCard = ({ card }) => {
               type="button"
             >
               View Details
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                aria-hidden="true"
-                className="h-4 w-4"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                ></path>
-              </svg>
+            
             </button>
           </a>
         </div>
