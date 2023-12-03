9a6efed97145d1acd442b2e3c2a98c45ac65324b
Musab Al Mahi <musabalmahi53@gmail.com>
2023-12-03 23:49:38 +0600

    donation card add and ridirect to daonation route

--- src/pages/Card/DonateCard.jsx
+++ src/pages/Card/DonateCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const DonateCard = ({ card }) => {
   const { id, image, title, description } = card || {};
 
@@ -5,9 +7,21 @@ const DonateCard = ({ card }) => {
   console.log(card);
   return (
     <div>
-      <img className="h-[450px] w-[900px] mx-auto" src={image} alt="" />
-      <h1 className="text-4xl font-bold">{title}</h1>
-      <p className="text-2xl ">{description}</p>
+      <img
+        className="h-[500px] w-[1000px] mx-auto relative"
+        src={image}
+        alt=""
+      />
+      <Link to="/donation">
+        <button className="btn top-full ml-36  bg-[#FF444A] absolute   focus:ring-4 focus:outline-none  dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          Donate $290
+        </button>
+      </Link>
+      <div className="my-10 px-32">
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-2xl ">{description}</p>
+        <p>An effective donation text message can help your organization reach its fundraising goals. Here are a few tips to help you write donation messages that convert.</p>
+      </div>
     </div>
   );
 };
